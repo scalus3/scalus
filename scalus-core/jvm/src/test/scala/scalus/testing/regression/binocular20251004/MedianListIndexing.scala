9a2584228c25b06376848d951ec601ee864bd32c
Alexander Nemish <anemish@gmail.com>
2025-11-05 12:22:43 +0100

    refactor: move scalus.regression to scalus.testing.regression

---
+++ scalus-core/jvm/src/test/scala/scalus/testing/regression/binocular20251004/MedianListIndexing.scala
@@ -0,0 +1,94 @@
+package scalus.testing.regression.binocular20251004
+
+import scalus.*
+import scalus.prelude.List
+import org.scalatest.funsuite.AnyFunSuite
+
+/** Regression test for "Cannot upcast Unit -> Int to Int" error
+  *
+  * Error occurs when compiling a function that:
+  *   1. Pattern matches on a List
+  *   2. Uses list indexing operator (!!) in one branch
+  *   3. Returns a value from the indexed element
+  *
+  * Original error from binocular project: scalus.sir.lowering.LoweringException: Cannot upcast Unit
+  * -> Int to Int at BitcoinValidator.scala:212 (getMedianTimePast function)
+  */
+class MedianListIndexing extends AnyFunSuite {
+
+    given scalus.Compiler.Options = scalus.Compiler.Options(
+      targetLoweringBackend = scalus.Compiler.TargetLoweringBackend.SirToUplcV3Lowering,
+      generateErrorTraces = true,
+      optimizeUplc = false,
+      debug = false
+    )
+
+    test("compile list indexing in pattern match - minimal reproducer") {
+        val sir = Compiler.compile {
+            def getMedian(values: List[BigInt], size: BigInt): BigInt =
+                values match
+                    case List.Nil => BigInt(0)
+                    case _ =>
+                        val index = size / 2
+                        values !! index
+
+            getMedian(List(BigInt(1), BigInt(2), BigInt(3)), BigInt(3))
+        }
+        println(s"sir:\n${sir.showHighlighted}")
+
+        // This should compile to UPLC without "Cannot upcast Unit -> Int to Int" error
+        sir.toUplcOptimized().plutusV3
+    }
+
+    test("compile list indexing without pattern match - control test") {
+        val sir = Compiler.compile {
+            val values = List(BigInt(1), BigInt(2), BigInt(3))
+            val index = BigInt(1)
+            values !! index
+        }
+
+        // This should work fine
+        sir.toUplcOptimized().plutusV3
+    }
+
+    test("compile pattern match without indexing - control test") {
+        val sir = Compiler.compile {
+            def getFirst(values: List[BigInt]): BigInt =
+                values match
+                    case List.Nil           => BigInt(0)
+                    case List.Cons(head, _) => head
+
+            getFirst(List(BigInt(1), BigInt(2), BigInt(3)))
+        }
+
+        // This should work fine
+        sir.toUplcOptimized().plutusV3
+    }
+
+    /*
+    test("compile original getMedianTimePast pattern - from binocular") {
+
+        val sir = Compiler.compile {
+            def getMedianTimePast(timestamps: List[BigInt], size: BigInt): BigInt =
+                val UnixEpoch = BigInt(1231006505)
+                timestamps match
+                    case List.Nil => UnixEpoch
+                    case _ =>
+                        val index = size / 2
+                        // List is sorted, so just get middle element
+                        timestamps !! index
+
+            val testTimestamps = List(
+              BigInt(1700000000),
+              BigInt(1700000600),
+              BigInt(1700001200)
+            )
+            getMedianTimePast(testTimestamps, BigInt(3))
+        }
+
+        // This reproduces the exact error from binocular project
+        sir.toUplcOptimized().plutusV3
+    }
+    
+     */
+}
---
+++ scalus-core/shared/src/test/scala/scalus/testing/regression/SumDatumValidator.scala
@@ -0,0 +1,48 @@
+package scalus.testing.regression
+
+import org.scalatest.funsuite.AnyFunSuite
+
+import scalus.*
+import scalus.builtin.{ByteString, Data, FromData, ToData}
+import scalus.ledger.api.v3.*
+import scalus.prelude.*
+
+@Compile
+object SumDatumValidator extends Validator:
+
+    enum TreasuryDatum:
+        case Unresolved(unresolvedDatum: ByteString)
+        case Resolved(resolvedDatum: ByteString)
+
+    given FromData[TreasuryDatum] = FromData.derived
+    given ToData[TreasuryDatum] = ToData.derived
+
+    inline override def spend(
+        datum: Option[Data],
+        redeemer: Data,
+        tx: TxInfo,
+        ownRef: TxOutRef
+    ): Unit =
+        //  val _: TreasuryDatum = datum match  // works
+        //  val x = datum match // works
+        val _ = datum match
+            case Option.Some(d) => d.to[TreasuryDatum]
+            case Option.None    => fail()
+
+end SumDatumValidator
+
+object SumDatumScript {
+    val sir = Compiler.compile(SumDatumValidator.validate)
+    def uplc = sir.toUplc(true)
+}
+
+class SumDatumValidatorTest extends AnyFunSuite:
+
+    test("SumDatumValidator should compile and transform to UPLC") {
+        pending
+        val script = SumDatumScript.uplc
+        // val result = script.evaluateDebug
+        // assert(result.isSuccess)
+    }
+
+end SumDatumValidatorTest
