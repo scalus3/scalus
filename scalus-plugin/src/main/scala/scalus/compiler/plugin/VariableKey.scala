a466aede5d49322b8878e720c83afde6edca3217
Ruslan Shevchenko <ruslan@shevchenko.kiev.ua>
2025-11-09 11:06:05 +0200

    fix/use '-' instead '#' for symbol suffix

--- scalus-plugin/src/main/scala/scalus/compiler/plugin/VariableKey.scala
+++ scalus-plugin/src/main/scala/scalus/compiler/plugin/VariableKey.scala
@@ -5,7 +5,7 @@ import dotty.tools.dotc.core.Symbols.Symbol
 /** Key for variable bindings that distinguishes shadowed variables by their symbols */
 case class VariableKey(displayName: String, symbolId: Option[Int]) {
     override def toString: String = symbolId match {
-        case Some(id) => s"$displayName#$id"
+        case Some(id) => s"$displayName-$id"
         case None     => displayName
     }
 
