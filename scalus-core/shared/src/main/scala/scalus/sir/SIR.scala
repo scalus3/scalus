0693edee9670924534dfd1aeb5364fb4799464b3
Alexander Nemish <anemish@gmail.com>
2025-11-09 10:46:54 +0100

    refactor: deprecate SIR and TargetLoweringBackend in favor of scalus.compiler.sir

--- scalus-core/shared/src/main/scala/scalus/sir/SIR.scala
+++ scalus-core/shared/src/main/scala/scalus/sir/SIR.scala
@@ -4,3 +4,8 @@ package scalus.sir
 type SIR = scalus.compiler.sir.SIR
 @deprecated("Use scalus.compiler.sir.SIR instead", "scalus 0.13.0")
 val SIR = scalus.compiler.sir.SIR
+
+@deprecated("Use scalus.compiler.sir.TargetLoweringBackend instead", "scalus 0.13.0")
+type TargetLoweringBackend = scalus.compiler.sir.TargetLoweringBackend
+@deprecated("Use scalus.compiler.sir.TargetLoweringBackend instead", "scalus 0.13.0")
+val TargetLoweringBackend = scalus.compiler.sir.TargetLoweringBackend
