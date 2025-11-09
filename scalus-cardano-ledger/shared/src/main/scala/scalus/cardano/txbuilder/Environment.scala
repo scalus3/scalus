494259c471127f94d14cf453d598b6851b0ea261
Alexander Nemish <anemish@gmail.com>
2025-11-09 18:46:39 +0100

    refactor: simplify Environment by using CardanoInfo directly

--- scalus-cardano-ledger/shared/src/main/scala/scalus/cardano/txbuilder/Environment.scala
+++ scalus-cardano-ledger/shared/src/main/scala/scalus/cardano/txbuilder/Environment.scala
@@ -1,19 +1,9 @@
 package scalus.cardano.txbuilder
 
-import scalus.cardano.address.Network
 import scalus.cardano.ledger.*
 
-case class Environment(
-    protocolParams: ProtocolParams,
-    slotConfig: SlotConfig,
-    network: Network,
-)
+type Environment = CardanoInfo
 
 object Environment {
-    def apply(cardanoInfo: CardanoInfo): Environment =
-        Environment(
-          protocolParams = cardanoInfo.protocolParams,
-          slotConfig = cardanoInfo.slotConfig,
-          network = cardanoInfo.network,
-        )
+    def apply(cardanoInfo: CardanoInfo): Environment = cardanoInfo
 }
