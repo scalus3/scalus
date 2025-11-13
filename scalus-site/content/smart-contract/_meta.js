87c8c2b1be8ea8ce383c3efb8dc34b476dc99028
otto <sae3023@gmail.com>
2025-11-13 18:31:01 +0100

    site: document validator development

--- scalus-site/content/smart-contract/_meta.js
+++ scalus-site/content/smart-contract/_meta.js
@@ -1,9 +1,8 @@
 export default {
     index: '',
-    // 'building-plutus-script': 'Building plutus script',
-    // 'evaluating-script': 'Evaluating script',
+    'developing-smart-contracts': 'Developing Smart Contracts',
     'in-depth-validator-look': 'In Depth Validator Walkthrough',
     'from-data': 'Extracting from Data',
-    'testing': 'Testing Validator',
-    'debugging': 'Troubleshooting',
+    'working-with-applications': 'Working with Applications',
+    'evaluating-script': 'Evaluating Scripts',
   }
