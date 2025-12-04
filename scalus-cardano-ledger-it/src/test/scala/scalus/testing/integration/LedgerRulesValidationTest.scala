33adcb60cf0d7e94cbab2c97037e70af2313f71f
Sergiy Shcherbyna <sergey.scherbina@gmail.com>
2025-12-04 11:33:55 +0100

    test: ledger rules blocks validation test

--- scalus-cardano-ledger-it/src/test/scala/scalus/testing/integration/LedgerRulesValidationTest.scala
+++ scalus-cardano-ledger-it/src/test/scala/scalus/testing/integration/LedgerRulesValidationTest.scala
@@ -1,17 +1,19 @@
 package scalus.testing.integration
 
 import org.scalatest.funsuite.AnyFunSuite
-import scalus.cardano.ledger.{OriginalCborByteArray, *}
 import scalus.cardano.ledger.rules.*
+import scalus.cardano.ledger.*
 import scalus.testing.integration.BlocksTestUtils.*
 
 import java.nio.file.Files
+import java.util.concurrent.atomic.AtomicInteger
 import scala.util.Try
 import scala.util.chaining.*
 
 class LedgerRulesValidationTest extends AnyFunSuite {
 
     test("validate transactions") {
+        val transactionsCount = AtomicInteger()
 
         val blocks = getAllBlocksPaths().take(1000)
 
@@ -26,6 +28,7 @@ class LedgerRulesValidationTest extends AnyFunSuite {
                 .transactions(using OriginalCborByteArray(bytes))
             utxos <- Try(scalusUtxoResolver.resolveUtxos(transaction)).toOption
             state = State(utxos = utxos)
+            _ = transactionsCount.incrementAndGet()
             result <- CardanoMutator
                 .transit(Context.testMainnet(), state, transaction)
                 .swap
@@ -41,7 +44,9 @@ class LedgerRulesValidationTest extends AnyFunSuite {
             .toSeq
             .sortBy(-_._2.length)
 
+        println(s"Transactions count: ${transactionsCount.get()}")
         println(s"Failed transactions: ${failed.size}")
+        println(s"Failures rate: ${(100 * failed.size) / transactionsCount.get()}%")
         println(s"\nFailures by exception type:")
         groups.foreach { case (exType, failures) =>
             println(s"  $exType: ${failures.length}")
