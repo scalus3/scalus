7595e487dccbe579374f918b5afe169fe1c6cf22
otto <sae3023@gmail.com>
2025-12-29 19:10:18 +0100

    docs: provider page

---
+++ scalus-site/content/ledger/_meta.js
@@ -0,0 +1,7 @@
+export default {
+  provider: 'Provider',
+  emulator: 'Emulator',
+  'protocol-parameters': 'Protocol Parameters',
+  'ledger-rules': 'Ledger Rules',
+  'yaci-devkit': 'Yaci DevKit',
+}
