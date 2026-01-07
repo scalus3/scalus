5033c9e04a14b05f45e9455fdd21112a784a8867
Alexander Nemish <anemish@gmail.com>
2026-01-07 12:17:33 +0100

    feat: expand TestPeer enum to 26 named parties (A-Z)
      Add cryptographically meaningful test party names including classic
      crypto roles (Eve/eavesdropper, Mallory/attacker, Peggy/prover,
      Victor/verifier, etc.) and blockchain-specific Oracle.

--- scalus-cardano-ledger/jvm/src/test/scala/scalus/cardano/txbuilder/TestPeer.scala
+++ scalus-cardano-ledger/jvm/src/test/scala/scalus/cardano/txbuilder/TestPeer.scala
@@ -17,16 +17,32 @@ import scalus.cardano.wallet.BloxbeanAccount
 import scala.collection.mutable
 
 enum TestPeer(@annotation.unused ix: Int) derives CanEqual:
-    case Alice extends TestPeer(0)
-    case Bob extends TestPeer(1)
-    case Carol extends TestPeer(2)
-    case Daniella extends TestPeer(3)
-    case Erin extends TestPeer(4)
-    case Frank extends TestPeer(5)
-    case Gustavo extends TestPeer(6)
-    case Hector extends TestPeer(7)
-    case Isabel extends TestPeer(8)
-    case Julia extends TestPeer(9)
+    case Alice extends TestPeer(0) // First party
+    case Bob extends TestPeer(1) // Second party
+    case Charles extends TestPeer(2) // Third party
+    case Dave extends TestPeer(3) // Fourth party
+    case Eve extends TestPeer(4) // Eavesdropper
+    case Faith extends TestPeer(5) // Trusted third party
+    case Grace extends TestPeer(6) // Government representative
+    case Heidi extends TestPeer(7)
+    case Ivan extends TestPeer(8) // Issuer
+    case Judy extends TestPeer(9) // Judge
+    case Kevin extends TestPeer(10)
+    case Laura extends TestPeer(11)
+    case Mallory extends TestPeer(12) // Malicious attacker
+    case Nathan extends TestPeer(13)
+    case Oracle extends TestPeer(14) // Blockchain oracle
+    case Peggy extends TestPeer(15) // Prover
+    case Quentin extends TestPeer(16)
+    case Rachel extends TestPeer(17)
+    case Sybil extends TestPeer(18) // Sybil attack
+    case Trent extends TestPeer(19) // Trusted third party
+    case Ursula extends TestPeer(20)
+    case Victor extends TestPeer(21) // Verifier
+    case Wendy extends TestPeer(22) // Whistleblower
+    case Xavier extends TestPeer(23)
+    case Yve extends TestPeer(24)
+    case Zulu extends TestPeer(25)
 
     def compareTo(another: TestPeer): Int = this.toString.compareTo(another.toString)
 
@@ -106,30 +122,62 @@ val genTestPeer: Gen[TestPeer] =
     Gen.oneOf(
       TestPeer.Alice,
       TestPeer.Bob,
-      TestPeer.Carol,
-      TestPeer.Daniella,
-      TestPeer.Erin,
-      TestPeer.Frank,
-      TestPeer.Gustavo,
-      TestPeer.Hector,
-      TestPeer.Isabel,
-      TestPeer.Julia
+      TestPeer.Charles,
+      TestPeer.Dave,
+      TestPeer.Eve,
+      TestPeer.Faith,
+      TestPeer.Grace,
+      TestPeer.Heidi,
+      TestPeer.Ivan,
+      TestPeer.Judy,
+      TestPeer.Kevin,
+      TestPeer.Laura,
+      TestPeer.Mallory,
+      TestPeer.Nathan,
+      TestPeer.Oracle,
+      TestPeer.Peggy,
+      TestPeer.Quentin,
+      TestPeer.Rachel,
+      TestPeer.Sybil,
+      TestPeer.Trent,
+      TestPeer.Ursula,
+      TestPeer.Victor,
+      TestPeer.Wendy,
+      TestPeer.Xavier,
+      TestPeer.Yve,
+      TestPeer.Zulu
     )
 
-/** Choose betweeen 2 and 10 peers */
+/** Choose between 2 and 26 peers */
 val genTestPeers: Gen[NonEmptyList[TestPeer]] =
     for {
-        numPeers <- Gen.choose(2, 10)
+        numPeers <- Gen.choose(2, 26)
         peersList = List(
           TestPeer.Alice,
           TestPeer.Bob,
-          TestPeer.Carol,
-          TestPeer.Daniella,
-          TestPeer.Erin,
-          TestPeer.Frank,
-          TestPeer.Gustavo,
-          TestPeer.Hector,
-          TestPeer.Isabel,
-          TestPeer.Julia
+          TestPeer.Charles,
+          TestPeer.Dave,
+          TestPeer.Eve,
+          TestPeer.Faith,
+          TestPeer.Grace,
+          TestPeer.Heidi,
+          TestPeer.Ivan,
+          TestPeer.Judy,
+          TestPeer.Kevin,
+          TestPeer.Laura,
+          TestPeer.Mallory,
+          TestPeer.Nathan,
+          TestPeer.Oracle,
+          TestPeer.Peggy,
+          TestPeer.Quentin,
+          TestPeer.Rachel,
+          TestPeer.Sybil,
+          TestPeer.Trent,
+          TestPeer.Ursula,
+          TestPeer.Victor,
+          TestPeer.Wendy,
+          TestPeer.Xavier,
+          TestPeer.Yve,
+          TestPeer.Zulu
         )
     } yield NonEmptyList.fromList(peersList.take(numPeers)).get
