32c8bd42bcf26f9ee51d42750b0d9dbe14cf547e
Alexander Nemish <anemish@gmail.com>
2026-01-07 23:46:48 +0100

    refactor: update import statements and use PlutusV3 for contract compilation in HelloCardanoTest

--- scalus-examples/jvm/src/test/scala/scalus/examples/HelloCardanoTest.scala
+++ scalus-examples/jvm/src/test/scala/scalus/examples/HelloCardanoTest.scala
@@ -2,7 +2,7 @@ package scalus.examples
 
 import org.scalatest.funsuite.AnyFunSuite
 import scalus.*
-import scalus.Compiler.compile
+import scalus.Compiler.TargetLoweringBackend
 import scalus.builtin.ByteString.*
 import scalus.builtin.Data
 import scalus.builtin.Data.toData
@@ -10,17 +10,23 @@ import scalus.cardano.ledger.ExUnits
 import scalus.ledger.api.v1.PubKeyHash
 import scalus.prelude.*
 import scalus.testing.kit.ScalusTest
+import scalus.uplc.PlutusV3
 
 import scala.language.implicitConversions
 
 class HelloCardanoTest extends AnyFunSuite with ScalusTest {
 
-    given scalus.Compiler.Options = scalus.Compiler.Options(
-      targetLoweringBackend = scalus.Compiler.TargetLoweringBackend.SirToUplcV3Lowering,
+    given Compiler.Options = Compiler.Options(
+      targetLoweringBackend = TargetLoweringBackend.SirToUplcV3Lowering,
       generateErrorTraces = true,
       optimizeUplc = true,
-      debug = false
     )
+
+    private val contract = PlutusV3.compile(HelloCardano.validate)
+
+    test(s"Hello Cardano script size is ${contract.script.script.size} bytes") {
+        assert(contract.script.script.size == 626)
+    }
 
     test("Hello Cardano") {
         val ownerPubKey = PubKeyHash(hex"1234567890abcdef1234567890abcdef1234567890abcdef12345678")
@@ -33,22 +39,18 @@ class HelloCardanoTest extends AnyFunSuite with ScalusTest {
 
         val compilerOptions = summon[Compiler.Options]
         val scalusBudget =
-            if compilerOptions.targetLoweringBackend == Compiler.TargetLoweringBackend.SirToUplcV3Lowering
+            if compilerOptions.targetLoweringBackend == TargetLoweringBackend.SirToUplcV3Lowering
             then {
                 // S3 lowering backend with lambda barriers (safer optimization)
                 if compilerOptions.generateErrorTraces then
                     ExUnits(memory = 31394L, steps = 10_253903)
-                else ExUnits(memory = 0L, steps = 0L)
+                else ExUnits(memory = 31094L, steps = 10_205903L)
             } else
                 //  Simple backend.  TODO: test for all backends
                 ExUnits(memory = 233876L, steps = 61_329752L)
 
-        val sir = compile(HelloCardano.validate)
-
-        // val lw = sir.toLoweredValue()
-        // println("lw: " + lw.show)
-
-        val result = sir.runScript(context)
+        val applied = contract.program $ context.toData
+        val result = applied.evaluateDebug
         assert(result.isSuccess)
         assert(result.budget == scalusBudget)
 
