0352f5831449051e599ed4fcc6081ce04726ea8b
Alexander Nemish <anemish@gmail.com>
2026-01-12 10:55:03 +0100

    test: add tests for PlutusV1, PlutusV2, and PlutusV3 compile and apply functionality

--- scalus-core/jvm/src/test/scala/scalus/uplc/CompiledTest.scala
+++ scalus-core/jvm/src/test/scala/scalus/uplc/CompiledTest.scala
@@ -9,11 +9,57 @@ import scalus.uplc.eval.PlutusVM
 
 class CompiledTest extends AnyFunSuite {
     private given Options = Options.release
-    private given PlutusVM = PlutusVM.makePlutusV3VM()
 
-    test("Compiled apply should work for compiled functions") {
-        val dataToBigInt = PlutusV3.compile((sc: Data) => sc.to[BigInt])
+    test("PlutusV1 compile and apply should work") {
+        given PlutusVM = PlutusVM.makePlutusV1VM()
+        val dataToBigInt = PlutusV1.compile((sc: Data) => sc.to[BigInt])
+        assert(dataToBigInt.language == scalus.cardano.ledger.Language.PlutusV1)
         assert(dataToBigInt(1.toData).program.term.evaluate == 1.asTerm)
         assert(dataToBigInt(1.toData).code == BigInt(1))
+    }
+
+    test("PlutusV2 compile and apply should work") {
+        given PlutusVM = PlutusVM.makePlutusV2VM()
+        val dataToBigInt = PlutusV2.compile((sc: Data) => sc.to[BigInt])
+        assert(dataToBigInt.language == scalus.cardano.ledger.Language.PlutusV2)
+        assert(dataToBigInt(2.toData).program.term.evaluate == 2.asTerm)
+        assert(dataToBigInt(2.toData).code == BigInt(2))
+    }
+
+    test("PlutusV3 compile and apply should work") {
+        given PlutusVM = PlutusVM.makePlutusV3VM()
+        val dataToBigInt = PlutusV3.compile((sc: Data) => sc.to[BigInt])
+        assert(dataToBigInt.language == scalus.cardano.ledger.Language.PlutusV3)
+        assert(dataToBigInt(3.toData).program.term.evaluate == 3.asTerm)
+        assert(dataToBigInt(3.toData).code == BigInt(3))
+    }
+
+    test("PlutusV1 should produce correct script hash prefix") {
+        val validator = PlutusV1.compile((_: Data) => ())
+        assert(validator.script.isInstanceOf[scalus.cardano.ledger.Script.PlutusV1])
+    }
+
+    test("PlutusV2 should produce correct script hash prefix") {
+        val validator = PlutusV2.compile((_: Data) => ())
+        assert(validator.script.isInstanceOf[scalus.cardano.ledger.Script.PlutusV2])
+    }
+
+    test("PlutusV3 should produce correct script hash prefix") {
+        val validator = PlutusV3.compile((_: Data) => ())
+        assert(validator.script.isInstanceOf[scalus.cardano.ledger.Script.PlutusV3])
+    }
+
+    test("withErrorTraces should work for all versions") {
+        val v1 = PlutusV1.compile((_: Data) => ())
+        val v2 = PlutusV2.compile((_: Data) => ())
+        val v3 = PlutusV3.compile((_: Data) => ())
+
+        assert(!v1.options.generateErrorTraces)
+        assert(!v2.options.generateErrorTraces)
+        assert(!v3.options.generateErrorTraces)
+
+        assert(v1.withErrorTraces.options.generateErrorTraces)
+        assert(v2.withErrorTraces.options.generateErrorTraces)
+        assert(v3.withErrorTraces.options.generateErrorTraces)
     }
 }
