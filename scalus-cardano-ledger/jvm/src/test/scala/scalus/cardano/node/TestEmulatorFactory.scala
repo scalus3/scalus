209669a66ea3ee805b539e24495c6c752a12f7a9
otto <sae3023@gmail.com>
2026-01-28 09:25:37 +0100

    factory of test emulators

---
+++ scalus-cardano-ledger/jvm/src/test/scala/scalus/cardano/node/TestEmulatorFactory.scala
@@ -0,0 +1,40 @@
+package scalus.cardano.node
+
+import org.bouncycastle.crypto.AsymmetricCipherKeyPair
+import org.bouncycastle.crypto.generators.Ed25519KeyPairGenerator
+import org.bouncycastle.crypto.params.{Ed25519KeyGenerationParameters, Ed25519PrivateKeyParameters, Ed25519PublicKeyParameters}
+import scalus.cardano.ledger.Utxos
+import scalus.cardano.ledger.rules.{Context, STS}
+import scalus.uplc.builtin.ByteString
+
+import java.security.SecureRandom
+
+object TestEmulatorFactory {
+    def create(
+        utxos: Utxos = Map.empty,
+        context: Context = Context(),
+        validators: Iterable[STS.Validator] = Emulator.defaultValidators,
+        mutators: Iterable[STS.Mutator] = Emulator.defaultMutators
+    ): EmulatorBase = new Emulator(utxos, context, validators, mutators)
+
+    /** Generate an Ed25519 key pair for testing.
+      * @return
+      *   (privateKey, publicKey) both as 32-byte ByteStrings
+      */
+    def generateKeyPair(): (ByteString, ByteString) = {
+        val asymmetricCipherKeyPair: AsymmetricCipherKeyPair = keyPairGenerator.generateKeyPair()
+        val privateKeyParams: Ed25519PrivateKeyParameters =
+            asymmetricCipherKeyPair.getPrivate.asInstanceOf[Ed25519PrivateKeyParameters]
+        val publicKeyParams: Ed25519PublicKeyParameters =
+            asymmetricCipherKeyPair.getPublic.asInstanceOf[Ed25519PublicKeyParameters]
+        val privateKey: ByteString = ByteString.fromArray(privateKeyParams.getEncoded)
+        val publicKey: ByteString = ByteString.fromArray(publicKeyParams.getEncoded)
+        (privateKey, publicKey)
+    }
+
+    private val keyPairGenerator = {
+        val keyPairGenerator = new Ed25519KeyPairGenerator()
+        keyPairGenerator.init(new Ed25519KeyGenerationParameters(new SecureRandom()))
+        keyPairGenerator
+    }
+}
