f21f280d255270981b2397853be5a3abeec905e3
fernweh0 <aleksey.khodakovskiy@gmail.com>
2026-03-16 10:18:56 +0100

    docs: split Merkle Trees into separate pages for static and incremental variants

--- scalus-site/content/advanced-data-structures/_meta.js
+++ scalus-site/content/advanced-data-structures/_meta.js
@@ -1,5 +1,6 @@
 export default {
-  'merkle-tree': 'Merkle Trees',
+  'merkle-tree': 'Merkle Tree',
+  'incremental-merkle-tree': 'Incremental Merkle Tree',
   'merkle-patricia-forestry': 'Merkle Patricia Forestry',
   'bilinear-accumulators': 'Bilinear Accumulators',
 }
